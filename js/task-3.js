a0c3393d4ba9f6dd6bd041a4481d5b7a322bff73
oleksandr-sulima <sulima.sasha@gmail.com>
2024-03-23 17:39:38 +0100

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,16 @@
-function getElementWidth(content, padding, border) {
-  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+function checkForSpam(message) {
+  let result = false;
+  const messageLowerCase = message.toLowerCase();
+  const saleCheck = messageLowerCase.includes("sale");
+  const spamCheck = messageLowerCase.includes("spam");
+  if (saleCheck || spamCheck) result = true;
+  return result;
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
